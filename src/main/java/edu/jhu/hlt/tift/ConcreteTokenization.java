eacce2e32975778635a2ecadce74cdeb8ec6586f
Max Thomas <max.thomas@jhu.edu>
2013-05-22 23:50:04 -0400

    added licensing info to top of various files

--- src/main/java/edu/jhu/hlt/tift/ConcreteTokenization.java
+++ src/main/java/edu/jhu/hlt/tift/ConcreteTokenization.java
@@ -160,9 +160,9 @@ public class ConcreteTokenization {
     public static int[] convertIntegers(List<Integer> integers) {
         int[] ret = new int[integers.size()];
         Iterator<Integer> iterator = integers.iterator();
-        for (int i = 0; i < ret.length; i++) {
+        for (int i = 0; i < ret.length; i++) 
             ret[i] = iterator.next().intValue();
-        }
+        
         return ret;
     }
 }
